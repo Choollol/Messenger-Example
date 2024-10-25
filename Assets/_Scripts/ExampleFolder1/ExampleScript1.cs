656465a732852bb1773721762e79b7d0f8c865cb
Christopher Sun <chrisun8@yahoo.com>
2024-10-25 11:59:07 -0700

    ObjectMessenger examples

--- Assets/_Scripts/ExampleFolder1/ExampleScript1.cs
+++ Assets/_Scripts/ExampleFolder1/ExampleScript1.cs
@@ -7,10 +7,17 @@ public class ExampleScript1 : MonoBehaviour
     private void OnEnable()
     {
         EventMessenger.StartListening("EventName", SomeFunction);
+        EventMessenger.StartListening("CreateObject", CreateObject);
     }
     private void OnDisable()
     {
         EventMessenger.StopListening("EventName", SomeFunction);
+        EventMessenger.StopListening("CreateObject", CreateObject);
+    }
+    private void CreateObject()
+    {
+        ObjectMessenger.SetGameObject("ObjectInstance", 
+            Instantiate(ObjectMessenger.GetGameObject("ObjectPrefab"), transform.position, Quaternion.identity));
     }
     private void SomeFunction()
     {
--- Assets/_Scripts/ExampleFolder2/ExampleScript3.cs
+++ Assets/_Scripts/ExampleFolder2/ExampleScript3.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ExampleScript3 : MonoBehaviour
 {
+    [SerializeField] private GameObject objectPrefab;
+    private void Awake()
+    {
+        ObjectMessenger.SetGameObject("ObjectPrefab", objectPrefab);
+        EventMessenger.TriggerEvent("CreateObject");
+        Debug.Log(ObjectMessenger.GetGameObject("ObjectInstance"));
+    }
     private void OnEnable()
     {
         EventMessenger.StartListening("EventName", AnotherFunction);
