a914ed7b3e89db977d051a543b1c50d5781bf0d3
Choollol <67297627+Choollol@users.noreply.github.com>
2025-01-14 20:32:49 -0800

    Added enum support

--- Assets/_Scripts/Messengers/DataMessenger.cs
+++ Assets/_Scripts/Messengers/DataMessenger.cs
@@ -6,50 +6,82 @@ using UnityEngine.Events;
 // Allows for communication of data between scripts, decoupling
 public class DataMessenger : MonoBehaviour
 {
+    private static Dictionary<string, bool> bools;
     private static Dictionary<string, float> floats;
     private static Dictionary<string, int> ints;
     private static Dictionary<string, string> strings;
     private static Dictionary<string, List<string>> stringLists;
-    private static Dictionary<string, bool> bools;
     private static Dictionary<string, Vector3> vector3s;
     private static Dictionary<string, Quaternion> quaternions;
     private static Dictionary<string, GameObject> gameObjects;
     private static Dictionary<string, ScriptableObject> scriptableObjects;
 
-    private static int defaultIntValue = 0;
-    private static float defaultFloatValue = 0;
-    private static bool defaultBoolValue = false;
-    private static string defaultStringValue = string.Empty;
-    private static List<string> defaultStringListValue = new List<string>();
-    private static Vector3 defaultVector3Value = Vector3.zero;
-    private static Quaternion defaultQuaternionValue = Quaternion.identity;
-    private static GameObject defaultGameObjectValue = null;
-    private static ScriptableObject defaultScriptableObjectValue = null;
+    private static readonly string DEFAULT_STRING = string.Empty;
+    private static readonly List<string> DEFAULT_STRING_LIST = new();
+    private static readonly Vector3 DEFAULT_VECTOR = Vector3.zero;
+    private static readonly Quaternion DEFAULT_QUATERNION = Quaternion.identity;
 
     private void Awake()
     {
+        bools = new Dictionary<string, bool>();
         floats = new Dictionary<string, float>();
         ints = new Dictionary<string, int>();
         strings = new Dictionary<string, string>();
         stringLists = new Dictionary<string, List<string>>();
-        bools = new Dictionary<string, bool>();
         vector3s = new Dictionary<string, Vector3>();
         quaternions = new Dictionary<string, Quaternion>();
         gameObjects = new Dictionary<string, GameObject>();
         scriptableObjects = new Dictionary<string, ScriptableObject>();
     }
+    public static bool GetBool(string key)
+    {
+        if (!bools.TryGetValue(key, out bool v))
+        {
+            bools[key] = default;
+            return bools[key];
+        }
+        return v;
+    }
+    public static bool GetBool(BoolKey key)
+    {
+        return GetBool(key.ToString());
+    }
+    public static void SetBool(string key, bool value)
+    {
+        bools[key] = value;
+    }
+    public static void SetBool(BoolKey key, bool value)
+    {
+        SetBool(key.ToString(), value);
+    }
+    public static void ToggleBool(string key)
+    {
+        bools[key] = !bools[key];
+    }
+    public static void ToggleBool(BoolKey key)
+    {
+        ToggleBool(key.ToString());
+    }
     public static float GetFloat(string key)
     {
         if (!floats.TryGetValue(key, out float v))
         {
-            floats[key] = defaultFloatValue;
-            return defaultFloatValue;
+            floats[key] = default;
+            return floats[key];
         }
         return v;
+    }
+    public static float GetFloat(FloatKey key)
+    {
+        return GetFloat(key.ToString());
     }
     public static void SetFloat(string key, float value)
     {
         floats[key] = value;
+    }
+    public static void SetFloat(FloatKey key, float value)
+    {
+        SetFloat(key.ToString(), value);
     }
     /// <summary>
     /// Performs an operation on the float associated with the given key with the value given. The operator is + by default.
@@ -70,18 +102,33 @@ public class DataMessenger : MonoBehaviour
         }
         return floats[key];
     }
+    /// <summary>
+    /// Performs an operation on the float associated with the given key with the value given. The operator is + by default.
+    /// </summary>
+    public static float OperateFloat(FloatKey key, float value, char op = '+')
+    {
+        return OperateFloat(key.ToString(), value, op);
+    }
     public static int GetInt(string key)
     {
         if (!ints.TryGetValue(key, out int v))
         {
-            ints[key] = defaultIntValue;
-            return defaultIntValue;
+            ints[key] = default;
+            return ints[key];
         }
         return v;
+    }
+    public static int GetInt(IntKey key)
+    {
+        return GetInt(key.ToString());
     }
     public static void SetInt(string key, int value)
     {
         ints[key] = value;
+    }
+    public static void SetInt(IntKey key, int value)
+    {
+        SetInt(key.ToString(), value);
     }
     /// <summary>
     /// Performs an operation on the int associated with the given key with the int given. The operator is + by default.
@@ -101,6 +148,13 @@ public class DataMessenger : MonoBehaviour
                 break;
         }
         return ints[key];
+    }
+    /// <summary>
+    /// Performs an operation on the int associated with the given key with the int given. The operator is + by default.
+    /// </summary>
+    public static int OperateInt(IntKey key, int value, char op = '+')
+    {
+        return OperateInt(key.ToString(), value, op);
     }
 
     /// <summary>
@@ -124,108 +178,190 @@ public class DataMessenger : MonoBehaviour
         }
         return ints[key];
     }
+    /// <summary>
+    /// Performs an operation on the int associated with the given key with the float given. 
+    /// </summary>
+    /// <param name="op">The operator is + by default.</param>
+    /// <param name="doRound">Truncates instead of rounds by default.</param>
+    public static int OperateInt(IntKey key, float value, char op = '+', bool doRound = false)
+    {
+        return OperateInt(key.ToString(), value, op, doRound);
+    }
 
     public static string GetString(string key)
     {
         if (!strings.TryGetValue(key, out string v))
         {
-            strings[key] = defaultStringValue;
-            return defaultStringValue;
+            strings[key] = DEFAULT_STRING;
+            return strings[key];
         }
         return v;
+    }
+    public static string GetString(StringKey key)
+    {
+        return GetString(key.ToString());
     }
     public static void SetString(string key, string value)
     {
         strings[key] = value;
     }
+    public static void SetString(StringKey key, string value)
+    {
+        SetString(key.ToString(), value);
+    }
     public static List<string> GetStringList(string key)
     {
         if (!stringLists.TryGetValue(key, out List<string> v))
         {
-            stringLists[key] = defaultStringListValue;
+            stringLists[key] = DEFAULT_STRING_LIST;
             return stringLists[key];
         }
         return v;
+    }
+    public static List<string> GetStringList(StringListKey key)
+    {
+        return GetStringList(key.ToString());
     }
     public static void SetStringList(string key, List<string> value)
     {
         stringLists[key] = value;
     }
+    public static void SetStringList(StringListKey key, List<string> value)
+    {
+        SetStringList(key.ToString(), value);
+    }
     public static void AddStringToList(string key, string value)
     {
-        if (!stringLists.TryGetValue(key, out List<string> v))
+        if (!stringLists.TryGetValue(key, out _))
         {
-            stringLists[key] = defaultStringListValue;
+            stringLists[key] = DEFAULT_STRING_LIST;
         }
         stringLists[key].Add(value);
     }
-    public static bool GetBool(string key)
+    public static void AddStringToList(StringListKey key, string value)
     {
-        if (!bools.TryGetValue(key, out bool v))
-        {
-            bools[key] = defaultBoolValue;
-            return defaultBoolValue;
-        }
-        return v;
-    }
-    public static void SetBool(string key, bool value)
-    {
-        bools[key] = value;
-    }
-    public static void ToggleBool(string key)
-    {
-        bools[key] = !bools[key];
+        AddStringToList(key.ToString(), value);
     }
     public static Vector3 GetVector3(string key)
     {
         if (!vector3s.TryGetValue(key, out Vector3 v))
         {
-            vector3s[key] = defaultVector3Value;
-            return defaultVector3Value;
+            vector3s[key] = DEFAULT_VECTOR;
+            return vector3s[key];
         }
         return v;
     }
+    public static Vector3 GetVector3(Vector3Key key)
+    {
+        return GetVector3(key.ToString());
+    }
+
     public static void SetVector3(string key, Vector3 value)
     {
         vector3s[key] = value;
+    }
+    public static void SetVector3(Vector3Key key, Vector3 value)
+    {
+        SetVector3(key.ToString(), value);
     }
     public static Quaternion GetQuaternion(string key)
     {
         if (!quaternions.TryGetValue(key, out Quaternion v))
         {
-            quaternions[key] = defaultQuaternionValue;
-            return defaultQuaternionValue;
+            quaternions[key] = DEFAULT_QUATERNION;
+            return quaternions[key];
         }
         return v;
+    }
+    public static Quaternion GetQuaternion(QuaternionKey key)
+    {
+        return GetQuaternion(key.ToString());
     }
     public static void SetQuaternion(string key, Quaternion value)
     {
         quaternions[key] = value;
     }
+    public static void SetQuaternion(QuaternionKey key, Quaternion value)
+    {
+        SetQuaternion(key.ToString(), value);
+    }
     public static GameObject GetGameObject(string key)
     {
         if (!gameObjects.TryGetValue(key, out GameObject obj))
         {
-            gameObjects[key] = defaultGameObjectValue;
-            return defaultGameObjectValue;
+            gameObjects[key] = default;
+            return gameObjects[key];
         }
         return obj;
+    }
+    public static GameObject GetGameObject(GameObjectKey key)
+    {
+        return GetGameObject(key.ToString());
     }
     public static void SetGameObject(string key, GameObject obj)
     {
         gameObjects[key] = obj;
     }
+    public static void SetGameObject(GameObjectKey key, GameObject obj)
+    {
+        SetGameObject(key.ToString(), obj);
+    }
     public static ScriptableObject GetScriptableObject(string key)
     {
         if (!scriptableObjects.TryGetValue(key, out ScriptableObject obj))
         {
-            scriptableObjects[key] = defaultScriptableObjectValue;
-            return defaultScriptableObjectValue;
+            scriptableObjects[key] = default;
+            return scriptableObjects[key];
         }
         return obj;
+    }
+    public static ScriptableObject GetScriptableObject(ScriptableObjectKey key)
+    {
+        return GetScriptableObject(key.ToString());
     }
     public static void SetScriptableObject(string key, ScriptableObject obj)
     {
         scriptableObjects[key] = obj;
     }
+    public static void SetScriptableObject(ScriptableObjectKey key, ScriptableObject obj)
+    {
+        SetScriptableObject(key.ToString(), obj);
+    }
+}
+
+public enum BoolKey
+{
+
+}
+public enum FloatKey
+{
+
+}
+public enum IntKey
+{
+
+}
+public enum StringKey
+{
+
+}
+public enum StringListKey
+{
+
+}
+public enum Vector3Key
+{
+
+}
+public enum QuaternionKey
+{
+
+}
+public enum GameObjectKey
+{
+
+}
+public enum ScriptableObjectKey
+{
+
 }
--- Assets/_Scripts/Messengers/EventMessenger.cs
+++ Assets/_Scripts/Messengers/EventMessenger.cs
@@ -63,6 +63,17 @@ public class EventMessenger : MonoBehaviour
     }
 
     /// <summary>
+    /// Associates a function with an event. Put this in OnEnable.
+    /// </summary>
+    /// <param name="eventName">The name of the event to be listened for. The same name will be used when triggering the event.</param>
+    /// <param name="listener">The function to be called when the event is triggered.</param>
+    /// <returns></returns>
+    public static void StartListening(EventKey eventName, UnityAction listener)
+    {
+        StartListening(eventName.ToString(), listener);
+    }
+
+    /// <summary>
     /// For every StartListening call, put a corresponding StopListening call in OnDisable too.
     /// </summary>
     /// <param name="eventName">The name of the event.</param>
@@ -76,6 +87,17 @@ public class EventMessenger : MonoBehaviour
         {
             thisEvent.RemoveListener(listener);
         }
+    }
+
+    /// <summary>
+    /// For every StartListening call, put a corresponding StopListening call in OnDisable too.
+    /// </summary>
+    /// <param name="eventName">The name of the event.</param>
+    /// <param name="listener">The function to stop listening to.</param>
+    /// <returns></returns>
+    public static void StopListening(EventKey eventName, UnityAction listener)
+    {
+        StopListening(eventName.ToString(), listener);
     }
 
     /// <summary>
@@ -94,4 +116,18 @@ public class EventMessenger : MonoBehaviour
             Debug.Log("EventMessenger does not contain " + eventName);
         }
     }
+
+    /// <summary>
+    /// Triggers an event through a given event name. All functions listening to the event will be invoked.
+    /// </summary>
+    /// <param name="eventName">The name of the event to trigger.</param>
+    public static void TriggerEvent(EventKey eventName)
+    {
+        TriggerEvent(eventName.ToString());
+    }
+}
+
+public enum EventKey
+{
+
 }
